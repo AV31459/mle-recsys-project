322f7fa88a940c2e768db40e318edefc8b7f207b
AV <andrey.volkov.77@yandex.ru>
2024-08-05 18:23:28 +0000

    wip

--- utils/s3.py
+++ utils/s3.py
@@ -17,6 +17,7 @@ def check_s3_credentials_in_environment():
         - S3_ENDPOINT_URL
     """
 
+    load_dotenv()
     for var_name in (
         'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'S3_BUCKET_NAME',
         'S3_ENDPOINT_URL'
@@ -26,26 +27,41 @@ def check_s3_credentials_in_environment():
                              'in environment')
 
 
-def print_bucket_contents(print_objects=True, file=sys.stdout):
+def get_client_and_bucket():
+    """
+    Returns boto3.client object and bucket_name according to credentials
+    set in environment.
+    """
+
+    # Проверяем креды
+    check_s3_credentials_in_environment()
+
+    return (
+        boto3.client(
+            service_name='s3',
+            endpoint_url=os.getenv('S3_ENDPOINT_URL'),
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+        ),
+        os.getenv('S3_BUCKET_NAME')
+    )
+
+
+def print_bucket_contents(print_objects=True,
+                          key_pattern='',
+                          file=sys.stdout):
     """
     Print contents of S3 bucket to a file (sys.stdout by default).
 
     NB: S3 bucket credentials should be set in environment.
     """
 
-    # Проверяем креды
-    check_s3_credentials_in_environment()
-
-    s3 = boto3.client(
-        service_name='s3',
-        endpoint_url=os.getenv('S3_ENDPOINT_URL'),
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-    )
-    bucket_name = os.getenv('S3_BUCKET_NAME')
+    s3, bucket_name = get_client_and_bucket()
     total_size_bytes = 0
 
-    print(f"Contents of bucket '{bucket_name}':", file=file)
+    print(f"Contents of bucket '{bucket_name}'", file=file)
+    if print_objects and key_pattern:
+        print(f'Using pattern "{key_pattern}" to filter displayed objects')
     if print_objects:
         print("-" * 40, file=file)
         print(f"{'File Name':<50} {'Size (MB)':>15}", file=file)
@@ -55,12 +71,48 @@ def print_bucket_contents(print_objects=True, file=sys.stdout):
     for obj in objects:
         file_size_gb = obj['Size'] / (1024 ** 2)  # Convert bytes to MB
         total_size_bytes += obj['Size']
-        if print_objects:
+        if print_objects and key_pattern in obj['Key']:
             print(f"{obj['Key']:<50} {file_size_gb:>15.2f} MB", file=file)
 
     print("-" * 40)
     total_size_gb = total_size_bytes / (1024 ** 3)  # Convert bytes to GB
-    print(f"Total Size: {total_size_gb:.2f} GB", file=file)
+    print(f"Total {len(objects)} objects, size: {total_size_gb:.2f} GB",
+          file=file)
+
+
+def upload_file_to_s3(local_file, s3_key):
+    """Uploads file to S3 bucket."""
+
+    s3_client, bucket_name = get_client_and_bucket()
+
+    s3_client.upload_file(
+        Filename=local_file,
+        Bucket=bucket_name,
+        Key=s3_key
+    )
+
+
+def delete_file_from_s3(s3_key):
+    """Deletes object (file) from s3 bucket."""
+
+    s3_client, bucket_name = get_client_and_bucket()
+
+    s3_client.delete_object(
+        Bucket=bucket_name,
+        Key=s3_key
+    )
+
+
+def download_file_from_s3(local_file, s3_key):
+    """Downloads file from s3 storage."""
+
+    s3_client, bucket_name = get_client_and_bucket()
+
+    s3_client.download_file(
+        Bucket=bucket_name,
+        Key=s3_key,
+        Filename=local_file
+    )
 
 
 if __name__ == '__main__':
